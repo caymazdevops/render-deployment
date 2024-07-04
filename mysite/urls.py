8124cc94d193f79cd426d647eee330164a43b27f
Yaşar Caymaz <110372249+caymazdevops@users.noreply.github.com>
2024-07-04 21:45:56 +0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('books/', include('book.urls')),
    # path('', include('blog.urls')),
     path('api/', include('blog.urls')),
-    path('api/', include('sports.urls')),
+    path('apis/', include('sports.urls')),
 ]
 
 
